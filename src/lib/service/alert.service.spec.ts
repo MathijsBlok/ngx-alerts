9c479fbf73fe1329a0956b34e372f41a1d9be394
Mathijs <mathijsblok@hotmail.com>
2018-02-01 08:37:07 +0100

    Update alert.service.spec.ts

--- src/lib/service/alert.service.spec.ts
+++ src/lib/service/alert.service.spec.ts
@@ -68,4 +68,52 @@ describe('AlertService', () => {
             expect(msg).toEqual(result);
         });
     }));
+    
+    it('should pop last alert', inject([AlertService], (service: AlertService) => {
+    service.danger('danger');
+    service.info('danger');
+    service.warning('danger');
+    service.success('danger');
+    service.danger('danger');
+    service.info('danger');
+    service.warning('danger');
+    service.success('danger');
+
+    service.messages.take(1).subscribe(msgs => {
+      console.log(msgs);
+      expect(msgs.length).toEqual(5);
+    });
+  }));
+
+  it('should close alert', inject([AlertService], (service: AlertService) => {
+    const result: Alert[] = [
+      {
+        content: 'danger',
+        type: 'warning'
+      }, {
+        content: 'danger',
+        type: 'info'
+      }, {
+        content: 'danger',
+        type: 'danger'
+      }
+    ];
+
+    service.danger('danger');
+    service.info('danger');
+    service.warning('danger');
+    service.success('danger');
+
+    service.messages
+      .take(1)
+      .subscribe(msgs => {
+        service.close(msgs[0]);
+
+        service.messages
+          .take(1)
+          .subscribe(msgs => {
+            expect(msgs).toEqual(result);
+          });
+      });
+  }));
 });
