666183bd2e3cc9b1ba46a7b1e56545d4b041bb13
Mathijs Blok <>
2018-05-13 10:58:28 +0200

    deleted stuff from wrong directory

--- src/lib/model/alert-action.model.ts
+++
@@ -1,8 +0,0 @@
-import {Alert} from './alert.model';
-import {AlertConfig} from './alert-config.model';
-
-export interface AlertAction {
-    alert: Alert;
-    type: 'ADD' | 'DELETE';
-    config: AlertConfig;
-}
